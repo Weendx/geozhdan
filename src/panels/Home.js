5c47f770d6c10f024b811d2898f1c673011e181e
YegorMerkulov <qsdsdfsd@bk.ru>
2023-07-14 16:11:52 +0300

    update interface 6

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -13,20 +13,22 @@ const Home = ({ id, go}) => {
 	})
 	return (
 	<Panel id={id}  style={{boxSizing: 'border-box'}}>
-		<Div className='main-panel'>
+		<div className='main-panel'>
 			<PanelHeader className='name-menu'>Главная</PanelHeader>
+			<Div>
 			<div className='info-panel'>
 				<Text className='info-name'>Открыто объектов</Text>
 				<Text className='info-text'>1 объект из 0</Text>
 				<Text className='info-name'>Дата последнего открытия</Text>
 				<Text className='info-text'>09 февраля 2022</Text>
 			</div>
+			</Div>
 			<div className='main__buttons'>
-				<Banner mode="image" asideMode='expand' className='banner' header="Карта лагеря" subheader="Открыть карту лагеря" background={<div style={{background: 'green'}} onClick={go} data-to='listOfPlaces'><Icon24Chevron style={{position: 'absolute', top: '50%', right: '16px', marginTop: '-12px'}}/></div>}></Banner>
-				<Banner mode="image" asideMode='expand' className='banner' header="Искать объекты" subheader="Открыть сканер QR-кода" background={<div style={{background: 'green'}} onClick={go} data-to='listOfPlaces'><Icon24Chevron style={{position: 'absolute', top: '50%', right: '16px', marginTop: '-12px'}}/></div>}></Banner>
-				<Banner mode="image" asideMode='expand' className='banner' header="Ваши открытия" subheader="Показать открытые объекты" background={<div style={{background: 'green'}} onClick={go} data-to='listOfPlaces'><Icon24Chevron style={{position: 'absolute', top: '50%', right: '16px', marginTop: '-12px'}}/></div>}></Banner>
+				<Banner mode="image" asideMode='expand' className='banner' header="Карта лагеря" subheader="Открыть карту лагеря" background={<div style={{background: 'green'}}><Icon24Chevron style={{position: 'absolute', top: '50%', right: '16px', marginTop: '-12px'}}/></div>} onClick={go} data-to='listOfPlaces'></Banner>
+				<Banner mode="image" asideMode='expand' className='banner' header="Искать объекты" subheader="Открыть сканер QR-кода" background={<div style={{background: 'green'}}><Icon24Chevron style={{position: 'absolute', top: '50%', right: '16px', marginTop: '-12px'}}/></div>} onClick={go} data-to='listOfPlaces'></Banner>
+				<Banner mode="image" asideMode='expand' className='banner' header="Ваши открытия" subheader="Показать открытые объекты" background={<div style={{background: 'green'}}><Icon24Chevron style={{position: 'absolute', top: '50%', right: '16px', marginTop: '-12px'}}/></div>} onClick={go} data-to='listOfPlaces'></Banner>
 			</div>
-		</Div>
+		</div>
 	</Panel>
 );
 }
