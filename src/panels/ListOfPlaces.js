190519a0775cc65e8b13c41e01807ebfaf844b20
YegorMerkulov <qsdsdfsd@bk.ru>
2023-07-14 21:27:57 +0300

    updata interface + pptx

--- src/panels/ListOfPlaces.js
+++ src/panels/ListOfPlaces.js
@@ -11,49 +11,6 @@ const ListOfPlaces = props => (
         </Group>
     </Panel>
 );
-const CustomPopout = ({ onClose }) => {
-    const { sizeX } = useAdaptivityConditionalRender();
-    return (
-      <PopoutWrapper onClick={onClose}>
-        <div
-          style={{
-            backgroundColor: 'var(--vkui--color_background_content)',
-            borderRadius: 8,
-            position: 'relative',
-            padding: '12px',
-          }}
-        >
-          <h4>Кастомное модальное окно</h4>
-  
-          {sizeX.regular && (
-            <ModalDismissButton className={sizeX.regular.className} onClick={onClose} />
-          )}
-        </div>
-      </PopoutWrapper>
-    );
-  };
-  
-  const Example = () => {
-    const [popout, setPopout] = React.useState(null);
-  
-    const onClick = () => setPopout(<CustomPopout onClose={() => setPopout(null)} />);
-  
-    return (
-      <SplitLayout popout={popout}>
-        <SplitCol>
-          <View activePanel="popout">
-            <Panel id="popout">
-              <PanelHeader>ModalDismissButton</PanelHeader>
-              <Group>
-                <CellButton onClick={onClick}>Открыть модальное окно</CellButton>
-              </Group>
-            </Panel>
-          </View>
-        </SplitCol>
-      </SplitLayout>
-    );
-};
-  
 
 ListOfPlaces.prototype = {
     id: PropTypes.string.isRequired,
