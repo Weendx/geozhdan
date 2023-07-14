79e1c2adea757a31eccf282f87836675636098de
Weendx <muha10bin@list.ru>
2023-07-15 00:24:55 +0300

    Add map on marker centering

--- src/panels/MapPanel.js
+++ src/panels/MapPanel.js
@@ -1,20 +1,24 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import PropTypes from 'prop-types';
 import { Panel, PanelHeader, PanelHeaderBack } from '@vkontakte/vkui';
 
 import Map from '../components/Map.js'
 
-const markers = [
+let markers = [
     {"name": "Marker Central", "posX": 56, "posY": 83, onClick: () => {alert("им. Адясова")}},
 	{"name": "Marker Adm", "posX": 55, "posY": 56.5},
     {"name": "Marker Warehouse", "posX": 80, "posY": 54},
-    {"name": "Marker ShipCafe", "posX": 10, "posY": 49}
+    {"name": "Marker ShipCafe", "posX": 10, "posY": 49},
+    {"name": "Marker Banya", "posX": 65, "posY": 141},
+    {"name": "Marker nast.tennis", "posX": 192, "posY": 115},
+    {"name": "zero", "posX": 0, "posY": 0}
 ];
 
 const MapPanel = props => {
 
     const headerRef = useRef(null);
     const mapRef = useRef(null);
+    const [mapShift, setShift] = useState({x: 10, y: 49});
 
     useEffect(() => {
         if (headerRef.current == null)
@@ -22,11 +26,14 @@ const MapPanel = props => {
         if (mapRef.current == null)
             mapRef.current = document.getElementById('mappanel__map');
         if (mapRef.current && headerRef.current) {
+            // setShift({x: 0, y: 0})
             console.log(mapRef, headerRef);
-            const width = document.body.offsetWidth;
-            const height = document.body.offsetHeight - headerRef.current.offsetHeight;
-            mapRef.current.style.width = width - 6 + 'px';
-            mapRef.current.style.height = height - 6 + 'px';
+            const padding = 6;
+            const width = document.body.offsetWidth - padding;
+            const height = document.body.offsetHeight - headerRef.current.offsetHeight - padding;
+            mapRef.current.style.width = width + 'px';
+            mapRef.current.style.height = height + 'px';
+            
         }
     }, [headerRef, mapRef]);
 
@@ -38,7 +45,7 @@ const MapPanel = props => {
 		>
 			Карта
 		</PanelHeader>
-		<Map id="mappanel__map" markers={markers}/>
+		<Map id="mappanel__map" markers={markers} centerAt={mapShift}/>
 	</Panel>
     );
 }
@@ -46,6 +53,7 @@ const MapPanel = props => {
 MapPanel.propTypes = {
 	id: PropTypes.string.isRequired,
 	go: PropTypes.func.isRequired,
+    mapCenterAt: PropTypes.array // {x: 0, y: 0}
 };
 
 export default MapPanel;
